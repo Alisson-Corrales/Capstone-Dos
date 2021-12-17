8a8f3b6f01daa84205d39cee4c05f4c2412b6e6d
buni <alisson.corrales24@gmail.com>
2021-12-16 18:28:27 -0700

    styling

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,6 +1,18 @@
-const sendEmail = (req, res) => {
-    res.send("email sent")
-    //const products = await Product.find({})
+
+
+const sendEmail = async (req, res) => {
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'ibgzpmfdphxhwf6r@ethereal.email',
+            pass: process.env.MAILER_PASS,
+        }
+    });
+
+    const info = await transporter.sendMail(message);
+
+    res.json(info);
 }
 
 module.exports = {sendEmail}
